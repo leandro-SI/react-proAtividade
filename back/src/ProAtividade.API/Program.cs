051d6fdf662383cbe7fd4804d01c50e2e76a45d4
Leandro <leandro.almeida@uvvnet.com.br>
2023-05-26 11:44:27 -0300

    Convertendo de .Net 5 para .Net 6

--- back/src/ProAtividade.API/Program.cs
+++ back/src/ProAtividade.API/Program.cs
@@ -1,26 +1,71 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+using ProAtividade.API.Data;
+using ProAtividade.Data.Repositories;
+using ProAtividade.Domain.Interfaces.Repositories;
+using ProAtividade.Domain.Interfaces.Services;
+using ProAtividade.Domain.Services;
+using System.Text.Json.Serialization;
 
-namespace ProAtividade.API
-{
-    public class Program
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddScoped<IAtividadeService, AtividadeService>();
+builder.Services.AddScoped<IAtividadeRepo, AtividadeRepositorio>();
+builder.Services.AddScoped<IGeralRepo, GeralRepositorio>();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(opt =>
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ProAtividade.API",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Leandro Cesar de Almeida",
+            Email = "leandro.almeida@uvvnet.com.br"
+        }
+    });
+});
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProAtividade.API v1"));
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors(opt => opt.AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowAnyOrigin());
+
+app.MapControllers();
+
+app.Run();
